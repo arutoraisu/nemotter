da95ff0d7aab3d4c9c65a1b4b4211da43b78cd12
arutoraisu <crayon6393@gmail.com>
2015-08-24 16:21:26 +0900

    Tag Serach & Date & Paging

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,11 +2,11 @@ class Message < ActiveRecord::Base
   belongs_to :team
   belongs_to :user
 
-  def self.search(search)
-    if search
-      Message.where(['name LIKE ?', "%#{search}%"])
-    else
-      Meassage.all
-    end
-  end
+  #  def self.search(search)
+  #    if search
+  #      Message.where(['name LIKE ?', "%#{search}%"])
+  #    else
+  #      Meassage.all
+  #    end
+  #  end
 end
