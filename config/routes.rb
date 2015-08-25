f065bb168f795c7dd9a8207aeb6cd1dc3d01af6c
12405 <12405@g.nagano-nct.ac.jp>
2015-08-25 13:46:04 +0900

    prototype

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :teams do
-    resources :messages, shallow: true 
+    resources :messages, shallow: true
       resources :team_users, shallow: true
   end
   # The priority is based upon order of creation: first created -> highest priority.
