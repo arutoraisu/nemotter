5eb24f2488dfcd37256a40ffbcf589895e583234
arutoraisu <crayon6393@gmail.com>
2015-08-25 12:25:44 +0900

    Tag Search

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,13 +1,4 @@
 class Message < ActiveRecord::Base
   belongs_to :team
   belongs_to :user
-
-
-   def self.search(search)
-      if search
-        Message.where(['name LIKE ?', "%#{search}%"])
-      else
-        Message.all
-      end
-    end
 end
