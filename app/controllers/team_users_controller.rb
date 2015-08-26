8f002f494c4ab4b5b3a7697575a532a8f8b8fd8b
12405 <12405@g.nagano-nct.ac.jp>
2015-08-26 17:20:32 +0900

    rechange team_id in team_users_controllers

--- app/controllers/team_users_controller.rb
+++ app/controllers/team_users_controller.rb
@@ -79,8 +79,7 @@ class TeamUsersController < ApplicationController
     end
 
     def set_team
-      #team_id = params[:team_id] || params[:team_users][:team_id] rescue team_id = 1
-      team_id =current_user.teams.first.try(:id)
+      team_id = params[:team_id] || params[:team_users][:team_id] rescue team_id = 1
       @team = Team.find(team_id)
     end
     # Never trust parameters from the scary internet, only allow the white list through.
