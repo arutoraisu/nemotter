b8729df61ad40e306352339a66cf4f0b6ffe1706
12405 <12405@g.nagano-nct.ac.jp>
2015-08-27 10:48:41 +0900

    active of sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!, :set_team
   #  before_action :set_team, only: [:index,:edit, :new, :show]
-  
+
     def set_team
       @team = current_user.teams.first rescue nil
       #team_id = params[:team_id] || params[:message][:team_id] rescue team_id = 1
       #@team = Team.find(team_id) rescue  @team = Team.first
+    end
+    def set_message
+      @message = Message.find(params[:id]) rescue nil
+    end
+    def set_team_user
+      @team_user = TeamUser.find(params[:id]) rescue nil
     end
 end
